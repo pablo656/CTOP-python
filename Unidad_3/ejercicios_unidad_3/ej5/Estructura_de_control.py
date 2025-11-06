cc047522ed2d6c4591e1ee1884d9bf1320c08f5e
pablo656 <pabloruizgonzalez45@gmail.com>
2025-11-06 17:53:37 +0100

    1

--- Unidad_3/ejercicios_unidad_3/ej5/Estructura_de_control.py
+++ Unidad_3/ejercicios_unidad_3/ej5/Estructura_de_control.py
@@ -1,7 +1,16 @@
-edad = int(input());
-if edad <= 18:
-    print("Eres menor de edad.");
-elif edad <= 18 and edad <= 64:
-    print("Eres adulto.");
-else:
-    print("Eres mayor.");
+try:
+    edad = int(input("introduce tu edad por favor"));
+except ValueError:
+    print("Por favor, ingresa un número válido para la edad.");
+try:
+    if edad <= 18 and edad >= 0:
+        print("Eres menor de edad.");
+    elif edad >= 18 and edad <= 64:
+        print("Eres adulto.");
+    elif edad >= 65:
+        print("Eres adulto mayor.");
+    else:
+        print("alienigena sal de aqui.");
+except Exception as e:
+    print("error acurrido: ",e);
+
